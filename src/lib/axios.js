058e905ebc52c6f27d95c7ab661941ee77fcd121
rasheedsaqib <mrasheed.bese19seecs@seecs.edu.pk>
2022-12-29 14:47:24 +0500

    url changed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: `http://localhost:4000/api`,
+    baseURL: `https://nust-feedback.vercel.app/api`,
 });
 
 instance.interceptors.response.use(
